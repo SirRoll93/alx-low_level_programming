79440381351083e38872119046d1807e137f4b7f
SirRoll93 <vincentasubonteng@gmail.com>
2022-09-08 11:48:38 -0700

    Size64

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Ending with a value of 0
+ */
+int main(void)
+{
+        printf("Size of a char: 1 byte\nSize of an int: 4 bytes\nSize of a long int: 8 bytes\nSize of a long long int: 8 bytes\nSize of a float: 4 bytes\n");
+
+        return (0);
+}
