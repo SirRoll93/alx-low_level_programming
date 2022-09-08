b4a37825a358238dc3499dab5047ed2dbab19222
SirRoll93 <vincentasubonteng@gmail.com>
2022-09-08 13:07:57 -0700

    Initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,17 @@
  */
 int main(void)
 {
-        printf("Size of a char: 1 byte\nSize of an int: 4 bytes\nSize of a long int: 8 bytes\nSize of a long long int: 8 bytes\nSize of a float: 4 bytes\n");
+        char size32[] = "Size of a char: 1 byte\nSize of an int: 4 bytes\n
+		Size of a long int: 4 bytes\nSize of a long long int: 8 bytes\n
+		Size of a float: 4 bytes\n";
+
+	char size64[] = "Size of a char: 1 byte\nSize of an int: 4 bytes\n
+		Size of a long int: 8 bytes\nSize of a long long int: 8 bytes\n
+		Size of a float: 4 bytes\n";
+
+        puts(size32);
+
+	puts(size64);
 
         return (0);
 }
