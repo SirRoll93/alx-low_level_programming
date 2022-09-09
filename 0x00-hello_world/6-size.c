be2fa0a84654144ee73ef26417259c1154857f66
SirRoll93 <vincentasubonteng@gmail.com>
2022-09-09 03:06:23 -0700

    My commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,6 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of float: %d byte(s)\n", sizeof(float));
+
 	return (0);
 }
