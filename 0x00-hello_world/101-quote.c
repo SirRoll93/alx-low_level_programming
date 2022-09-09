9be3aed1a8f522169ba2d79a9035a4f376238f32
SirRoll93 <vincentasubonteng@gmail.com>
2022-09-09 04:57:34 -0700

    My commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
