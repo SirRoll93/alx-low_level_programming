463c54ebcbfd33aa4850c614eb49cef45041aa11
SirRoll93 <vincentasubonteng@gmail.com>
2022-09-10 02:17:54 +0000

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,25 +7,23 @@
 
 int main(void)
 {
- int n, m;
+	int n, m;
 
- for (n = 48; n <= 56; n++)
- {
-  for (m = 49; m <= 57; m++)
-  {
-   if (m > n)
-   {
-    putchar(n);
-    putchar(m);
-    if (n != 56 || m != 57)
-    {
-     putchar(',');
-     putchar(' ');
-    }
-   }
-  }
- }
- putchar('\n');
- return (0);
+	for (n = 48; n <= 56; n++)
+	{
+	for (m = 49; m <= 57; m++)
+	{
+		if (m > n)
+		{
+			putchar(n);
+			putchar(m);
+		}
+		if (n != 56 || m != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
