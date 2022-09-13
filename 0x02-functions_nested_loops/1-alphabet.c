410b3296595ff3c6d9cfea1c7acc17cf548aafaa
SirRoll93 <vincentasubonteng@gmail.com>
2022-09-13 15:10:34 -0700

    My commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet - function to print abc
  *
- * Return: 0
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -13,4 +13,6 @@ void print_alphabet(void)
 		_putchar(c);
 	}
 _putchar('\n');
+
+return (0);
 }
