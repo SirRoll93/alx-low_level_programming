b70ce303c0ea0141c8599ad2adeba51cb4c93cfb
SirRoll93 <vincentasubonteng@gmail.com>
2022-09-13 15:35:13 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *function that checks for lowercase character
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a'; && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
