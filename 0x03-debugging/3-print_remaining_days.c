0398821c5650d05f42e66ead0fd038b50d5a5518
SirRoll93 <vincentasubonteng@gmail.com>
2022-09-15 17:19:40 -0700

    My commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,7 @@ void print_remaining_days(int month, int day, int year)
 	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 	{
 		if (month > 2)
-			day++
+			day++;
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
