5b4800258fa074a9fa1df577b335774ac09ee25f
SirRoll93 <vincentasubonteng@gmail.com>
2022-09-21 15:55:12 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0.a function that concatenates two strings.\
+1.a function that concatenates two strings.\
+2.a function that copies a string.\
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Description: Header file containing prototypes for all functions
+ * used in the 0x06-pointers_arrays_strings directory
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char c);
+.
