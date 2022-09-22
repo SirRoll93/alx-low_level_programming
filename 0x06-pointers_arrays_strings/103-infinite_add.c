e0c423c0cf475e097b770e50090485b93858c91e
SirRoll93 <vincentasubonteng@gmail.com>
2022-09-22 04:18:57 -0700

    In progress

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *infinite_add - adds two numbers
+ * @n1: number to be added
+ * @n2: number to be added
+ * @r: the buffer that the function will use to store the result
+ * @size_r: is the buffer size
+ *
+ * Return: Always 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+{
+
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_buffer - prints buffer
+ * @b: character pointed
+ * @size: bytes of the buffer
+ *
+ * Return: Always 0
+ */
+void print_buffer(char *b, int size);
+{
+       char *b	
+}
