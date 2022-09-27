28ff2c0585df62c1c7aaabd75419620f1850dd1b
SirRoll93 <vincentasubonteng@gmail.com>
2022-09-27 01:05:47 +0000

    Progressing

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,31 +1,34 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring.
+ * *_strspn - gets the length of a prefix substring.
  * @s: main string to be scanned
  * @accept: string containing the list of characters to natch
- * Return: results
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	unsigned int result = 0;
+	int i, j, f, flag;
 
-	for (i = 0; s[i] != ' '; i++)
+	f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
+		flag = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				result++;
-				break;
+				f++;
+				flag = 1;
 			}
 		}
-		if (accept[j] == '\0')
+		if (flag == 0)
 		{
-			return (result);
+			return (f);
 		}
 	}
 
-	return (results);
+	return (0);
 }
